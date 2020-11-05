56b9bdc3c060166c1bc8cf7fe3f3c739a6b6ffe2
Oscar <Oscar040509@gmail.com>
2020-11-05 23:31:30 +0800

    設定icon

--- hashtagsAll.php
+++ hashtagsAll.php
@@ -12,6 +12,7 @@ include 'Find.php';
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Tables - SB Admin</title>
+        <link href="images/logo-rainbow.png"  rel="icon">
         <link href="css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
--- userDelete.php
+++ userDelete.php
@@ -13,6 +13,7 @@ include 'Find.php';
         <meta name="author" content="" />
         <title>Static Navigation - SB Admin</title>
         <link href="css/styles.css" rel="stylesheet" />
+        <link href="images/logo-rainbow.png"  rel="icon">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
         <script src="assets/js/sweetalert.min.js" type="text/javascript"></script>
 

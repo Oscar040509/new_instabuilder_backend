dbd5cb8a794810faa6f415f77f810a2a63475a88
Oscar <Oscar040509@gmail.com>
2020-11-06 18:01:17 +0800

    navbar-brand 後端管理

--- hashtagsAll.php
+++ hashtagsAll.php
@@ -19,7 +19,7 @@ include 'Find.php';
         
     <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="userIndex.php">Instabuilder</a>
+            <a class="navbar-brand" href="userIndex.php">Instabuilder 後端管理</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <!-- Navbar-->
